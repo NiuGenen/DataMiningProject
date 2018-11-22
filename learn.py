69e5eebe0f54070eabfc31ed199085cb6f0edb7b
NiuGenen <602131568@qq.com>
2018-11-22 15:16:26 +0800

    clean useless file

--- learn.py
+++ learn.py
@@ -1,4 +1,10 @@
 import sklearn.tree as tree
+import dm_csv as dmcsv
+import pandas as pd
 
-for i in range(10,10):
-    print(i)
+asd = pd.read_csv("./asd.csv", sep=',')
+zxc = pd.read_csv("./zxc.csv", sep=',')
+qwe = pd.concat([asd,zxc])
+dmcsv.write_list2_into_csv( qwe.values, ['asd1','asd2','asd3'], "./qwe1.csv", 1)
+qwe = pd.concat([asd,zxc], ignore_index=True)
+dmcsv.write_list2_into_csv( qwe.values, ['asd1','asd2','asd3'], "./qwe2.csv", 1)
