82e4b98d38262034af2b1d7c6e800319d77b4eb7
NiuGenen <602131568@qq.com>
2018-12-03 01:14:26 +0800

    correlation analysis function

--- dm_preprocess_fun.py
+++ dm_preprocess_fun.py
@@ -32,37 +32,44 @@ def process_source_file(csv, train_data, filename, data_cols, label_col, verbose
         print( train_data )
     return train_data
 
-def pp_correlation_analysis_categorical( list2, col1id, col1name, col2id, col2name):
-    col1 = []
-    col2 = []
-    col1_dict = dict()
-    col2_dict = dict()
-    col1_cnt = 0
-    col2_cnt = 0
+def pp_correlation_analysis_categorical( list2, colids, colnames):
+    nr = colids.__len__()
+    cols = []
+    cols_dict = []
+    cols_cnt = []
+    for i in range(0,nr):
+        cols.append( [] )
+        cols_dict.append( dict() )
+        cols_cnt.append( 0 )
     for item in list2:
-        if not col1_dict.__contains__( item[col1id] ):
-            col1_dict[ item[col1id] ] = col1_cnt
-            col1_cnt += 1
-        if not col2_dict.__contains__( item[col2id] ):
-            col2_dict[ item[col2id] ] = col2_cnt
-            col2_cnt += 1
-        col1.append( col1_dict[item[col1id]] )
-        col2.append( col2_dict[item[col2id]] )
-    pds1 = pd.Series(data=col1, name=col1name)
-    pds2 = pd.Series(data=col2, name=col2name)
-    df = pd.concat([pds1, pds2], axis=1)
-    return df.corr()
+        i = 0
+        for id in colids:
+            dct = cols_dict[i]
+            if not dct.__contains__( item[id] ):
+                dct[ item[id] ] = cols_cnt[i]
+                cols_cnt[i] += 1
+            cols[i].append( dct[item[id]] )
+            i += 1
+    pds0 = pd.Series(data=cols[0], name=colnames[0])
+    for i in range(1,nr):
+        pdsi = pd.Series(data=cols[i], name=colnames[i])
+        pds0 = pd.concat([pds0, pdsi], axis=1)
+    return pds0.corr()
 
-def pp_correlation_analysis( list2, col1id, col1name, col2id ,col2name, col1v, col2v):
-    col1 = []
-    col2 = []
+def pp_correlation_analysis( list2, colids, colnames):
+    nr = colids.__len__()
+    cols = []
+    for i in range(0,nr):
+        cols.append( [] )
     for item in list2:
-        col1.append( item[col1id] )
-        col2.append( item[col2id] )
-    pds1 = pd.Series(data=col1, name=col1name)
-    pds2 = pd.Series(data=col2, name=col2name)
-    df = pd.concat([pds1, pds2], axis=1)
-    return df.corr()
+        i = 0
+        for id in colids:
+            cols[i].append( item[id] )
+    pds0 = pd.Series(data=cols[0], name=colnames[0])
+    for i in range(1,nr):
+        pdsi = pd.Series(data=cols[i], name=colnames[i])
+        pds0 = pd.concat([pds0, pdsi], axis=1)
+    return pds0.corr()
 
 def pp2_taging(csv, colname, value, tag):
     i = 0
